7047347216686754a2c97a8daa152a415371fb72
rex <rexshao@gmail.com>
2025-06-07 00:11:36 +0800

    修复des对象获取BUG

--- tellyget/utils/cipher.py
+++ tellyget/utils/cipher.py
@@ -2,8 +2,15 @@ from Crypto.Cipher import DES3
 from Crypto.Util.Padding import pad, unpad
 
 class Cipher:
-    def __init__(self, key):
-        self.cipher = DES3.new(key.encode(), DES3.MODE_ECB)
+    def __init__(self, MDK):
+        if MDK[:8] == MDK[8:16] and MDK[-8:] == MDK[8:16]:
+            self.cipher = DES.new(MDK[:8], DES.MODE_ECB)
+        elif MDK[:8] == MDK[8:16]:
+            self.cipher = DES3.new(MDK[8:], DES3.MODE_ECB)
+        elif MDK[-8:] == MDK[8:16]:
+            self.cipher = DES3.new(MDK[:-8], DES3.MODE_ECB)
+        else:
+            self.cipher = DES3.new(MDK, DES3.MODE_ECB)
 
     def encrypt(self, plain_text):
         cipher_text = self.cipher.encrypt(pad(plain_text.encode(), DES3.block_size))
